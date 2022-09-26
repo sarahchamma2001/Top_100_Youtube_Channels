6a7dba3f1389cb931c69e46f5c99c7ee28b8d590
sarahchamma2001 <107916244+sarahchamma2001@users.noreply.github.com>
2022-09-26 11:25:29 +0300

    Update Visualization.py

--- Visualization.py
+++ Visualization.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 import requests
 import inspect
-from streamlit_lottie import st_lottie
+#from streamlit_lottie import st_lottie
 from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 from numerize import numerize
